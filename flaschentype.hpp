b3ad284bdafac81ac63c33f8f3ae2d40e3eb3417
Manuel <manuel.winkler@gmx.ch>
2024-05-08 16:24:31 +0200

    Typst

--- flaschentype.hpp
+++ flaschentype.hpp
@@ -1,11 +1,23 @@
 #ifndef FLASCHENTYPE_HPP
 #define FLASCHENTYPE_HPP
+#include <cmath>
+#include <fltcfg.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include <array>
 #include <memory>
+#include <iostream>
+#include <fstream>
 #include <cassert>
+#include <vector>
+#include <cstdlib>
 #include <unordered_map>
+#ifdef ENABLE_TYPST
+#include <lunasvg.h>
+#endif
+
+
+
 struct Character {
     std::array<int, 2>   Size;      // Size of glyph
     std::array<int, 2>   Bearing;   // Offset from baseline to left/top of glyph
@@ -30,6 +42,38 @@ struct text_image{
         return buffer[i * w + j];
     }
 };
+#ifdef ENABLE_TYPST
+inline text_image draw_text_typst(std::string text, uint32_t size_in_pt, std::string fontname = "", std::vector<std::string> more_options = {}){
+    using namespace std::string_literals;
+    const std::string fn = "yf"s;
+    {
+        std::ofstream out("/tmp/" + fn + ".typ");
+        out << "#set text(" << size_in_pt << "pt)\n";
+        out << "#set text(top-edge: \"bounds\", bottom-edge: \"bounds\")\n";
+        if(!fontname.empty())
+            out << "#set text(font: \"" << fontname << "\"pt)\n";
+        out << "#set page(width:auto, height: auto, margin: 0pt)\n";
+        for(auto str : more_options){
+            out << str << "\n";
+        }
+        out << text << "\n";
+    }
+    std::system(("typst c /tmp/" + fn + ".typ -f svg").c_str());
+
+    std::unique_ptr<lunasvg::Document> doc = lunasvg::Document::loadFromFile("/tmp/" + fn + ".svg");
+    std::cout << doc->width() << " x ";
+    std::cout << doc->height() << "\n";
+    int w = std::ceil(doc->box().w);
+    int h = std::ceil(doc->box().h);
+    lunasvg::Bitmap bm = doc->renderToBitmap(doc->width(), doc->height());
+    bm.convertToRGBA();
+    text_image ret(bm.width(), bm.height());
+    for(int i = 0;i < bm.width() * bm.height();i++){
+        ret.buffer[i] = bm.data()[i * 4 + 3];
+    }
+    return ret;
+}
+#endif
 void draw_glyph_at(text_image& img, int x, int baseline_descent, const Character& c);
 int GetCodepointNext(const char *text, int *codepointSize);
 struct Font{
